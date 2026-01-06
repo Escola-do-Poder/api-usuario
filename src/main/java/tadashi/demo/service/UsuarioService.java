ba8ac8be94d5a812632ba6e62aa12f7462b87419
Gabriell <gabrielltadashi123@gmail.com>
2026-01-06 18:52:53 -0300

    feat(): Código atualizado no git remoto

---
+++ src/main/java/tadashi/demo/service/UsuarioService.java
@@ -0,0 +1,49 @@
+package tadashi.demo.service;
+
+
+import tadashi.demo.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tadashi.demo.repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> buscar() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).get();
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario atualizar(Long id, Usuario usuario) {
+        Usuario existente = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        existente.setNome(usuario.getNome());
+        existente.setEmail(usuario.getEmail());
+
+
+        return usuarioRepository.save(existente);
+    }
+
+    public void deletar(Long id) {
+        if (!usuarioRepository.existsById(id)) {
+            throw new RuntimeException("Usuário não encontrado");
+        }
+        usuarioRepository.deleteById(id);
+
+    }
+}
+
